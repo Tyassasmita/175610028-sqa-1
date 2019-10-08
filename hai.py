606b56358cef07107c232a23a691d44dbe5b4816
novendrasatria <novendradotid@gmail.com>
2019-10-08 16:02:32 +0700

    Upload Hai.py, Semoga sukses YaaAllah

---
+++ hai.py
@@ -0,0 +1,9 @@
+Python 2.7 (r27:82525, Jul  4 2010, 09:01:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print"Good Morning"
+Good Morning
+>>> print"It's Friday!"
+It's Friday!
+>>> print"Stay positive and have a great day uwu!!"
+Stay positive and have a great day uwu!!
+>>> 
